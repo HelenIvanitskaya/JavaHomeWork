59a4f6f06cc5912f46aedae3582f3b684623bbbf
elena <elena.bandorina@privatbank.ua>
2021-10-15 14:42:02 +0300

    1nit commit

---
+++ src/com/pb/ivanitskaya/hm2/Calculator.java
@@ -0,0 +1,22 @@
+package com.pb.ivanitskaya.hm2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int operand1;
+        int operand2;
+        String sign;
+        System.out.println("Введите первое целое число" );
+        operand1 = in.nextInt();
+        System.out.println("Введите второе целое число" );
+        operand2 = in.nextInt();
+        System.out.println("Введите знак арифметической операции");
+        sign = in.next();
+
+      }
+
+
+    }
+}
